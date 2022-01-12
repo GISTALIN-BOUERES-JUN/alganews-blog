12b790c1b3e4f0e40d58d6aa29b4406400c6baec
Geraldo Boueres <geraldoboueres@gmail.com>
2022-01-12 15:13:24 -0400

    Listing Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import FeaturedPost from "../components/FeaturedPost";
 import { ServerResponse } from "http";
+import PostCard from "../components/PostCard";
 
 interface HomeProps {
   posts?: Post.Paginated;
@@ -19,6 +20,9 @@ export default function Home(props: HomeProps) {
       </Head>
 
       {posts?.content && <FeaturedPost postSummary={posts?.content[0]} />}
+      {posts?.content?.slice(1).map((post) => {
+        return <PostCard key={post.id} post={post} />;
+      })}
     </div>
   );
 }
